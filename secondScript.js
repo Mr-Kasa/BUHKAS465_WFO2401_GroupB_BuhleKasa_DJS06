69ac54dd160c2938f5e1c9f1a51d463f087cba40
Mr-Kasa <156632133+Mr-Kasa@users.noreply.github.com>
2024-05-24 12:17:24 +0200

    crreated a function to log products with no price

--- secondScript.js
+++ secondScript.js
@@ -8,29 +8,45 @@ const products = [
     { product: 'tea', price: '' },
   ]
 
-/**
- * Question 2
- * 
- */
- /**
-     * logging each product name
-     */
- const productNames = products.map(products => {
-    return products.product
- })
+// Question 2
+ 
 
+ /**
+ * Logging each product name
+ */
+const productNames = () => products.map(product => {
+    return product.product;
+});
 
  /**
   * logging products with names longer than 5 characters
   */
- const productsNameLongerThan5 = products.filter(product => product.product.length >= 5)
-const filteredNames = productsNameLongerThan5.map(product => product.product);
+ const filteredNames = ()=>{
+ const productsNameLongerThan5 = products.filter(product => product.product.length > 5)
+  return productsNameLongerThan5.map(product => product.product);
+ }
+
+
+/**
+ * logging product prices with no price
+ */
+const productsWithNoPrice = ()=>{
+    const filteredProducts = products.filter(price=> price.price==false)
+    return filteredProducts.map(product => product.product )
+}
+
+/**
+ * Concects all product names into a single string
+ */
+
 
 
 
 console.log(
-        productNames,
+        productNames(),
         "\n",
-        filteredNames,
+        filteredNames(),
+        '\n',
+        productsWithNoPrice()
 
 )
