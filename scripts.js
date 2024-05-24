6dc3ef4afdcaf43ec2ac91c20d8091123eb05d5f
Mr-Kasa <156632133+Mr-Kasa@users.noreply.github.com>
2024-05-24 09:11:30 +0200

    created aan object with names and provinces.

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@ const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', '
 const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']
 
 // A list of products with prices:
+// eslint-disable-next-line no-unused-vars
 const products = [
   { product: 'banana', price: "2" },
   { product: 'mango', price: 6 },
@@ -13,6 +14,12 @@ const products = [
   { product: 'coffee', price: 10 },
   { product: 'tea', price: '' },
 ]
+
+/**
+ * Question 1
+ * 
+ */
+
 
 //List of names
 console.log('Names:')
@@ -40,14 +47,29 @@ console.log('Name and Province')
 nameAndProvince(names, provinces)
 
 
+
+console.log('\n')
+console.log('Provinces in uppercase')
+
+//Turn provinces into a string
+function upperCase(word){
+    const uncapitalized = word
+     const capitalized = uncapitalized.toUpperCase()
+     return capitalized
+}
+console.log(provinces.map(upperCase))
+
+
+
+
 //Number of letters in each name
   
-function Capitalizer(name){
+function stringLength(name){
   return name.length
 }
 console.log('\n')
 console.log("An array containing the number of letters in each name respectively")
-console.log(names.map(Capitalizer))
+console.log(names.map(stringLength))
 
 //Sorting Provinces alphabetically
 console.log('\n')
@@ -56,14 +78,38 @@ console.log(provinces.sort())
 
 
 
-//Removing provinces with the word Cape From the List
-function capeRemover() {
-  const filteredArray = provinces.filter(province => !province.includes('Cape'));
+//Removing provinces with the word Cape From the List and
+function capeProvinceRemoval() {
+  const filteredArray = provinces.filter(province => !(province.includes('Cape')));
   console.log('\n');
-  console.log('Provinces array after provinces with "Cape" are removed:');
-  console.log(filteredArray);
+  console.log('Number of provinces after provinces with "Cape" are removed:');
+  console.log(filteredArray.length);
 }
 
-capeRemover();
+capeProvinceRemoval()
+console.log('\n')
+console.log('Check if names contain S, Returns true or false')
+//create bollean value if province name contains 'S'
+function contains_S(word){
+    return word+"   "+(word.toUpperCase()).includes('S')
+}
+console.log(names.map(contains_S))
 
-//
+
+
+//Transform names array into an object
+console.log('\n')
+console.log('Object of names and provinces')
+let nameAndProvinceObject = names.reduce((acc, name, index) => {
+  acc[name] = provinces[index];
+  return acc;
+}, {});
+
+console.log(nameAndProvinceObject);
+
+
+/**
+ * Question 2
+ * 
+ */
+
